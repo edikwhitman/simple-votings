59cad46b4052d1ea993e30560add9b536cdbe05d
Edward Whitman <edikwhitman@yandex.ru>
2019-01-03 15:27:33 +0700

    some changes and added static where it should be

--- simple_votings/settings.py
+++ simple_votings/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'vote',
+    'vote.apps.VoteConfig',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
