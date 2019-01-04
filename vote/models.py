7f9a705787717afa8944a4aa22c582cffc5c0926
Edward Whitman <edikwhitman@yandex.ru>
2019-01-04 17:47:20 +0700

    addede vote model

--- vote/models.py
+++ vote/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Vote(models.Model):
+    name = models.CharField(max_length=100, default='')
+    text = models.CharField(max_length=300, default='')
+    options = models.CharField(max_length=500, default='')
