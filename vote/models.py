eb2a10fc3935057269b54f17b5e4b04668bf9d46
Уитман Эдвард <edikwhitman@yandex.ru>
2019-01-06 13:59:37 +0300

    Admin

--- vote/models.py
+++ vote/models.py
@@ -1,9 +1,15 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
 
 
 class Vote(models.Model):
-    name = models.CharField(max_length=100, default='')
-    text = models.CharField(max_length=300, default='')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular voting")
+    question = models.CharField(max_length=100, default='')
     options = models.CharField(max_length=500, default='')
+
+    def __str__(self):
+        return self.question
