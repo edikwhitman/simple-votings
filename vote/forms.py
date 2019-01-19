fa12b0e5aae6d3c3dab4369b832a4526530ac6d7
Edward Whitman <edikwhitman@yandex.ru>
2019-01-19 19:06:14 +0700

    Removed first and last name from registration

--- vote/forms.py
+++ vote/forms.py
@@ -3,8 +3,6 @@ from django import forms
 
 class SignInForm(forms.Form):
     user_name = forms.CharField(label="Логин:", widget=forms.TextInput(attrs={'class': 'float-right'}))
-    user_fname = forms.CharField(label="Имя:", widget=forms.TextInput(attrs={'class': 'float-right'}))
-    user_lname = forms.CharField(label="Фамилия:", widget=forms.TextInput(attrs={'class': 'float-right'}))
     user_email = forms.EmailField(label="Email:", widget=forms.TextInput(attrs={'class': 'float-right'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'float-right'}), label="Пароль:")
     password_conf = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'float-right'}),
--- vote/views.py
+++ vote/views.py
@@ -120,16 +120,13 @@ def sign_up(request):
 
         if f.is_valid():
             u_n = f.data['user_name']
-            u_fn = f.data['user_fname']
-            u_ln = f.data['user_lname']
             u_em = f.data['user_email']
             u_pw = f.data['password']
             u_pw_c = f.data['password_conf']
 
             if not f_m.is_existing_user(User.objects.all(), u_n):
                 if u_pw == u_pw_c:
-                    new_user = User.objects.create_user(username=u_n, email=u_em, password=u_pw,
-                                                        first_name=u_fn, last_name=u_ln)
+                    new_user = User.objects.create_user(username=u_n, email=u_em, password=u_pw)
                     new_user.save()
                     return HttpResponseRedirect('/login/')
                 else:
@@ -144,7 +141,7 @@ def sign_up(request):
         f = SignInForm()
         context['form'] = f
 
-    return render(request, 'registration/sign_in.html', context)
+    return render(request, 'registration/sign_up.html', context)
 
 
 @login_required
